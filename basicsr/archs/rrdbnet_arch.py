b2bb731ed9835b9fb18bc568f2ce48ac5d37cb85
Kevin <xiangyukong0720@gmail.com>
2025-08-25 23:08:57 +0800

    Add playground.py

--- basicsr/archs/rrdbnet_arch.py
+++ basicsr/archs/rrdbnet_arch.py
@@ -99,6 +99,6 @@ class RRDBNet(nn.Module):
         feat = feat + body_feat
         # upsample
         feat = self.lrelu(self.conv_up1(F.interpolate(feat, scale_factor=2, mode='nearest')))
-        feat = self.lrelu(self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
+        # feat = self.lrelu(self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
         out = self.conv_last(self.lrelu(self.conv_hr(feat)))
         return out
